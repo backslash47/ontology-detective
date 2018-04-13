04617f53416710fe734b697d948c024f9c951442
Matus Zamborsky <zamborsky@gmail.com>
2018-04-13 20:56:40 +0200

    remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '~/app/app';
-import registerServiceWorker from '~/registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
@@ -27,4 +26,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
